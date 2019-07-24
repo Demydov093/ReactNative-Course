14e7776a2a200ffe23595a833b05d96350c89e28
Dmytriy Demydov <Dmytriy_Demydov@epam.com>
2019-07-24 11:24:50 +0300

    native

---
+++ App.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+
+export default class App extends Component {
+
+    state = {
+        places: []
+    }
+
+    placeAddedHandler = placeName => {
+        this.setState(prevState => {
+            return {
+                places: prevState.places.concat(placeName)
+            };
+        });
+    };
+
+  render() {
+    return (
+        <View style={styles.center}>
+            <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
+            <PlaceList places={this.state.places}/>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+     center: {
+       flex: 1,
+       padding: 26,
+       justifyContent: 'flex-start',
+       alignItems: 'center',
+     },
+});
+
