74d30c7d2b9b064d57f9b9d56f74679de2d5c3c0
Dmytriy Demydov <Dmytriy_Demydov@epam.com>
2019-07-24 13:58:09 +0300

    'redux-init'

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import {StyleSheet, ScrollView} from "react-native";
+import {StyleSheet, FlatList} from "react-native";
 import ListItem from "../ListItem/ListItem";
 
 const placeList = props => {
-    const placeOutput = props.places.map((place, index) => (
-        <ListItem
+    /*const placeOutput = props.places.map((place, index) => (*/
+        {/*<ListItem
             key={index}
             placeName={place}
-            onItemPressed={() => props.onItemDeleted(index)}/>
-    ))
-    return (
-        <ScrollView style={styles.listContainer}>
+            onItemPressed={() => props.onItemDeleted(index)}/>*/}
+    /*))*/
+    return <FlatList
+        style={styles.listContainer}
+        data={props.places}
+        renderItem={(info) => (
+            <ListItem
+                placeName={info.item.value}
+                placeImage={info.item.image}
+                onItemPressed={() => props.onItemDeleted(info.item.key)}/>
+        )}
+    />
+    /*return (
+        {/!*<ScrollView style={styles.listContainer}>
             {placeOutput}
-        </ScrollView>
-    )
+        </ScrollView>*!/}
+    )*/
 };
 
 const styles = StyleSheet.create({
